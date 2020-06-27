86d566e2b5fdd07b713bf0d8e2c0db4ca5d2a198
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-06-28 00:01:41 +0530

    Update game.js

--- game.js
+++ game.js
@@ -4,6 +4,12 @@ let countDown = document.createElement("div");
 countDown.setAttribute("id", "countdown");
 base.setAttribute("style", "height:75%; width: 75%");
 
+let panel = document.createElement('div')
+panel.setAttribute("id", "panel");
+let scoreBoard = document.createElement('div')
+scoreBoard.setAttribute("id", "scoreboard");
+let heading = document.createElement('h1')
+heading.innerHTML = "MINESWEEPER"
 //making layout
 
 function box() {
@@ -16,8 +22,8 @@ function box() {
 let id = 0;
 function square(left, top) {
   let newSquare = document.createElement("div");
-  newSquare.style.left = left * 2 + "px";
-  newSquare.style.top = top * 2 + "px";
+  newSquare.style.left = left * 3 + "px";
+  newSquare.style.top = top * 3 + "px";
   newSquare.setAttribute("class", "boxes");
   newSquare.setAttribute("id", `${id}`);
   base.appendChild(newSquare);
@@ -35,7 +41,7 @@ window.onload = function setBombs() {
   }
 
   for (let i = 0; i < 40; i++) {
-    document.getElementById(`${arr[i]}`).innerHTML = "X";
+    document.getElementById(`${arr[i]}`).value = "X";
   }
 };
 
@@ -45,52 +51,52 @@ function setValues() {
   for (let i = 0; i < 256; i++) {
     count = 0;
     let bb = document.getElementById(`${i}`);
-    if (bb.innerHTML != "X") {
+    if (bb.value != "X") {
       // continue;
 
       // else
       if (
         document.getElementById(`${i - 17}`) != null &&
-        document.getElementById(`${i - 17}`).innerHTML == "X"
+        document.getElementById(`${i - 17}`).value == "X"
       )
         count++;
       if (
         document.getElementById(`${i - 16}`) != null &&
-        document.getElementById(`${i - 16}`).innerHTML == "X"
+        document.getElementById(`${i - 16}`).value == "X"
       )
         count++;
       if (
         document.getElementById(`${i - 15}`) != null &&
-        document.getElementById(`${i - 15}`).innerHTML == "X"
+        document.getElementById(`${i - 15}`).value == "X"
       )
         count++;
       if (
         document.getElementById(`${i - 1}`) != null &&
-        document.getElementById(`${i - 1}`).innerHTML == "X"
+        document.getElementById(`${i - 1}`).value == "X"
       )
         count++;
       if (
         document.getElementById(`${i + 1}`) != null &&
-        document.getElementById(`${i + 1}`).innerHTML == "X"
+        document.getElementById(`${i + 1}`).value == "X"
       )
         count++;
       if (
         document.getElementById(`${i + 15}`) != null &&
-        document.getElementById(`${i + 15}`).innerHTML == "X"
+        document.getElementById(`${i + 15}`).value == "X"
       )
         count++;
       if (
         document.getElementById(`${i + 16}`) != null &&
-        document.getElementById(`${i + 16}`).innerHTML == "X"
+        document.getElementById(`${i + 16}`).value == "X"
       )
         count++;
       if (
         document.getElementById(`${i + 17}`) != null &&
-        document.getElementById(`${i + 17}`).innerHTML == "X"
+        document.getElementById(`${i + 17}`).value == "X"
       )
         count++;
     }
-    if (count != 0) bb.innerHTML = count;
+    if (count != 0) bb.value = count;
   }
 }
 
@@ -101,13 +107,15 @@ function addClick() {
   for (let i = 0; i < 256; i++) {
     document.getElementById(`${i}`).addEventListener("click", (e) => {
     
-      if (document.getElementById(`${i}`).innerHTML == "X") {
+      if (document.getElementById(`${i}`).value == "X") {
+        document.getElementById(`${i}`).innerHTML = document.getElementById(`${i}`).value
         document.getElementById(`${i}`).style.backgroundColor = "red"
         alert("game over");
         location.reload();
       }
 
       clean(i)
+      points()
     });
   }
 }
@@ -115,20 +123,26 @@ function addClick() {
 // pseudo code! main beast code took 5 hours! i am dumb kill me!
 function clean(i){
 
-    if(document.getElementById(`${i}`).innerHTML == 'X')
-    return;
+    if(document.getElementById(`${i}`).value == 'X'){
+        document.getElementById(`${i}`).innerHTML = document.getElementById(`${i}`).value
+        return;
+    }
+    
 
-    if( document.getElementById(`${i}`).innerHTML!=""){
+    if( document.getElementById(`${i}`).value!=undefined){
         document.getElementById(`${i}`).style.backgroundColor = "green"
+        document.getElementById(`${i}`).innerHTML = document.getElementById(`${i}`).value
         return
     }
     
   //
   if(document.getElementById(`${i}`).style.backgroundColor == "green")
   return
-    if(document.getElementById(`${i}`).innerHTML==""){
+    if(document.getElementById(`${i}`).value==undefined){
        
         document.getElementById(`${i}`).style.backgroundColor = "green"
+        if(document.getElementById(`${i}`).value==undefined)
+        document.getElementById(`${i}`).innerHTML = ""
         if(i%16==0){
             clean(i-16)
             clean(i-15)
@@ -187,6 +201,24 @@ function clean(i){
 // }
 setTimeout(addClick, 1000);
 
+
+
+
+let score;
+// add points
+
+function points(){
+    score = 0;
+    for(let i = 0; i<256; i++){
+        let green = document.getElementById(`${i}`)
+        if(green.style.backgroundColor == "green")
+        score++
+    }
+    scoreBoard.innerHTML = "SCORE: " + score;
+}
+
+scoreBoard.innerHTML = "SCORE: 0"
+
 //countdown
 countDown.innerHTML = 005 + ":" + 002;
 startTimer();
@@ -201,7 +233,7 @@ function startTimer() {
   }
 
   countDown.innerHTML = m + ":" + s;
-  console.log(m);
+  //console.log(m);
   setTimeout(startTimer, 1000);
 }
 
@@ -215,5 +247,11 @@ function checkSecond(sec) {
   return sec;
 }
 
+//side panel DOM
+
+panel.appendChild(countDown)
+panel.appendChild(scoreBoard)
+
+document.body.appendChild(heading)
 document.body.appendChild(base);
-//document.body.appendChild(countDown);
+document.body.appendChild(panel);
