e26264c280a400653e4687e973fd46be584aeaa7
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-06-28 05:01:12 +0530

    added difficulty levels

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-document.body.setAttribute('style',"background-image: url('bg.jpg'); height: 100%; width: 100%")
+window.onload = alert("PLEASE SWITCH TO FULL SCREEN MODE ON A DESKTOP FOR BEST EXPERIENCE")
 
-let start = document.createElement('button')
-let link = document.createElement('a')
-link.setAttribute('href','game.html')
-start.innerHTML = "START"
+document.body.setAttribute('style',"background-image: url('bg.jpg'); height: 100%; width: 100%")
+//easy
+let startEasy = document.createElement('button')
+let linkEasy = document.createElement('a')
+linkEasy.setAttribute('href','easy.html')//chose easy, medium hard
+startEasy.innerHTML = "EASY"
+//medium
+let startMedium = document.createElement('button')
+let linkMedium = document.createElement('a')
+linkMedium.setAttribute('href','game.html')//chose easy, medium hard
+startMedium.innerHTML = "MEDIUM"
+//hard
+let startHard = document.createElement('button')
+let linkHard = document.createElement('a')
+linkHard.setAttribute('href','hard.html')//chose easy, medium hard
+startHard.innerHTML = "HARD"
+
+//
 let btn = document.createElement('div')
 btn.setAttribute('id','btn')
 let title = document.createElement('h1')
@@ -13,7 +27,14 @@ title.innerHTML = "MINESWEEPER GAME"
 
 let heading = document.createElement('div')
 heading.appendChild(title)
-link.appendChild(start)
-btn.appendChild(link)
+
+linkEasy.appendChild(startEasy)
+linkMedium.appendChild(startMedium)
+linkHard.appendChild(startHard)
+
+btn.appendChild(linkEasy)
+btn.appendChild(linkMedium)
+btn.appendChild(linkHard)
+
 document.body.appendChild(heading)
 document.body.appendChild(btn)
