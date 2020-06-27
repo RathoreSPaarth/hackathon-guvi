5522c60d24e52d03db0a0750931ee9a907997d8f
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2020-06-28 03:11:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,21 +4,16 @@ let start = document.createElement('button')
 let link = document.createElement('a')
 link.setAttribute('href','game.html')
 start.innerHTML = "START"
-let div = document.createElement('div')
+let btn = document.createElement('div')
+btn.setAttribute('id','btn')
 let title = document.createElement('h1')
 title.innerHTML = "MINESWEEPER GAME"
 // //start.setAttribute('style',"height: 20%; width: 50%; border: none; border-radius: 2%; background-color: #7295cc; z-index:1; position: absolute;")
 // start.setAttribute('onclick',"href = 'game.html'")
 
-
-
-
-
-
-
-
-
+let heading = document.createElement('div')
+heading.appendChild(title)
 link.appendChild(start)
-div.appendChild(link)
-document.body.appendChild(title)
-document.body.appendChild(div)
+btn.appendChild(link)
+document.body.appendChild(heading)
+document.body.appendChild(btn)
